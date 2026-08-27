398ef8157f8104e25b6fb19365c4c7138767ff38
hjfisher <280495460+hjfisher@users.noreply.github.com>
2026-08-27 15:45:17 +0330

    release: v1.1.0

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ import (
 	"snispf-hj-go/internal/utils"
 )
 
-const version = "1.0.0"
+const version = "1.1.0"
 
 const banner = `
 ███████╗███╗   ██╗██╗███████╗██████╗ ███████╗    ██╗  ██╗     ██╗       ██████╗  ██████╗ 
